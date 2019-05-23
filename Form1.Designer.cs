c3b785a92f4b53cfd6a1c54073dc42cd7b451b9c
pologan <pgandorski@gmail.com>
2019-05-23 16:11:20 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace CompareImages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCompare = new System.Windows.Forms.Button();
+            this.txtBoxOpnFile1 = new System.Windows.Forms.TextBox();
+            this.txtBoxOpnFile2 = new System.Windows.Forms.TextBox();
+            this.BtnOpnFile1 = new System.Windows.Forms.Button();
+            this.BtnOpnFile2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnCompare
+            // 
+            this.BtnCompare.Location = new System.Drawing.Point(366, 151);
+            this.BtnCompare.Name = "BtnCompare";
+            this.BtnCompare.Size = new System.Drawing.Size(75, 23);
+            this.BtnCompare.TabIndex = 0;
+            this.BtnCompare.Text = "Porównaj";
+            this.BtnCompare.UseVisualStyleBackColor = true;
+            this.BtnCompare.Click += new System.EventHandler(this.BtnCompare_Click);
+            // 
+            // txtBoxOpnFile1
+            // 
+            this.txtBoxOpnFile1.Location = new System.Drawing.Point(12, 35);
+            this.txtBoxOpnFile1.Name = "txtBoxOpnFile1";
+            this.txtBoxOpnFile1.Size = new System.Drawing.Size(296, 20);
+            this.txtBoxOpnFile1.TabIndex = 1;
+            // 
+            // txtBoxOpnFile2
+            // 
+            this.txtBoxOpnFile2.Location = new System.Drawing.Point(492, 35);
+            this.txtBoxOpnFile2.Name = "txtBoxOpnFile2";
+            this.txtBoxOpnFile2.Size = new System.Drawing.Size(296, 20);
+            this.txtBoxOpnFile2.TabIndex = 2;
+            // 
+            // BtnOpnFile1
+            // 
+            this.BtnOpnFile1.Location = new System.Drawing.Point(12, 61);
+            this.BtnOpnFile1.Name = "BtnOpnFile1";
+            this.BtnOpnFile1.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpnFile1.TabIndex = 3;
+            this.BtnOpnFile1.Text = "Przeglądaj...";
+            this.BtnOpnFile1.UseVisualStyleBackColor = true;
+            this.BtnOpnFile1.Click += new System.EventHandler(this.BtnOpnFile1_Click);
+            // 
+            // BtnOpnFile2
+            // 
+            this.BtnOpnFile2.Location = new System.Drawing.Point(492, 61);
+            this.BtnOpnFile2.Name = "BtnOpnFile2";
+            this.BtnOpnFile2.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpnFile2.TabIndex = 4;
+            this.BtnOpnFile2.Text = "Przeglądaj...";
+            this.BtnOpnFile2.UseVisualStyleBackColor = true;
+            this.BtnOpnFile2.Click += new System.EventHandler(this.BtnOpnFile2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pierwszy obraz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(489, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Drugi obraz:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(366, 205);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(96, 13);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "Obraz podobny w: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 267);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnOpnFile2);
+            this.Controls.Add(this.BtnOpnFile1);
+            this.Controls.Add(this.txtBoxOpnFile2);
+            this.Controls.Add(this.txtBoxOpnFile1);
+            this.Controls.Add(this.BtnCompare);
+            this.Name = "Form1";
+            this.Text = "Comparing Images";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCompare;
+        private System.Windows.Forms.TextBox txtBoxOpnFile1;
+        private System.Windows.Forms.TextBox txtBoxOpnFile2;
+        private System.Windows.Forms.Button BtnOpnFile1;
+        private System.Windows.Forms.Button BtnOpnFile2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompareImages
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnOpnFile1_Click(object sender, EventArgs e)
+        {
+            txtBoxOpnFile1.Clear();
+            OpenFileDialog opnDlg = new OpenFileDialog();
+            opnDlg.ShowDialog();
+            string str = opnDlg.FileName;
+            txtBoxOpnFile1.AppendText(str);
+
+        }
+
+        private void BtnOpnFile2_Click(object sender, EventArgs e)
+        {
+            txtBoxOpnFile2.Clear();
+            OpenFileDialog opnDlg = new OpenFileDialog();
+            opnDlg.ShowDialog();
+            txtBoxOpnFile2.AppendText(opnDlg.FileName);
+        }
+
+        private void BtnCompare_Click(object sender, EventArgs e)
+        {
+            if((txtBoxOpnFile1.Text != null)&&(txtBoxOpnFile2.Text != null))
+            {
+                double entropySumFirst, entropySumSecond;
+                entropySumFirst = EntropyCounting(txtBoxOpnFile1.Text);
+                entropySumSecond = EntropyCounting(txtBoxOpnFile2.Text);
+                Compare(entropySumFirst, entropySumSecond);
+            }else
+            {
+                MessageBox.Show("Nie podano ścieżki do obrazu");
+            }
+        }
+
+        private double EntropyCounting(string filePath)
+        {
+            Image img = Image.FromFile(filePath);
+            double entropySum = 0; //selfexplaining - propably should be list too 
+            List<int> pixelsArray = new List<int>(); //List of RGB Data
+            int[] counter = new int[256]; //Count how many times specific number apear in 128-elements
+            using (Bitmap bmp = new Bitmap(img))
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        //Adding RGB info to array
+                        Color clr = bmp.GetPixel(x, y);
+                        pixelsArray.Add(clr.R);
+                        pixelsArray.Add(clr.G);
+                        pixelsArray.Add(clr.B);
+                    }
+                }
+            }
+            //Counting Shannon's Entropy for every 128 
+            int section = (int)pixelsArray.Count / 128;
+            for (int j = 0; j < section; j++)
+            {
+                for (int i = (128 * j); i <= 127 + 128 * j; i++)
+                {
+                    //Counting amount of elements of specific number
+                    int num = pixelsArray[i];
+                    ++counter[num];
+                }
+                for (int i = 128 * j; i <= 127 + 128 * j; i++)
+                {
+                    if (counter[pixelsArray[i]] != 0)
+                    {
+                        entropySum += ((double)counter[pixelsArray[i]] / 128) * Math.Log(128 / counter[pixelsArray[i]], 2);
+                    }
+                }
+                Array.Clear(counter, 0, counter.Length);
+
+                
+            }
+            entropySum = entropySum / section;
+            return entropySum;
+        }
+        private void Compare(double entropySumFirst, double entropySumSecond)
+        {
+            double result;
+            if (entropySumFirst > entropySumSecond)
+            {
+                result = entropySumSecond / entropySumFirst * 100;
+            }else
+            {
+                result = entropySumFirst / entropySumSecond * 100;
+            }
+            MessageBox.Show("Obrazy podobne w " + result.ToString() + "%");
+        }
+    }
+}
